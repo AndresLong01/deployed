fbc38ebc16610ca5fd7fe9fc24c67fcb719390fb
Andres Long <andreslong95@gmail.com>
2020-04-01 23:34:19 -0500

    Updates

---
+++ precache-manifest.909bfc32d02cb4c80f5b6645ace949ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a9cd22c8d0524844363ad7e7b3d182",
+    "url": "/deployed/index.html"
+  },
+  {
+    "revision": "006b37759374c8a83f51",
+    "url": "/deployed/static/css/main.996d1b58.chunk.css"
+  },
+  {
+    "revision": "05e9f3ada5200431711c",
+    "url": "/deployed/static/js/2.ce6dec20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deployed/static/js/2.ce6dec20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006b37759374c8a83f51",
+    "url": "/deployed/static/js/main.df070c6a.chunk.js"
+  },
+  {
+    "revision": "5c8145a92537d2152247",
+    "url": "/deployed/static/js/runtime-main.d8b95461.js"
+  }
+]);
